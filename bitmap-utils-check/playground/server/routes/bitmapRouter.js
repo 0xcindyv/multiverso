42dca456bd0652e81551731c581e4539a9fe0e2a
0xcindyv <cindysanford@gmail.com>
2025-03-10 00:30:07 -0300

    Remove bitmap-utils-check directory and add Dependabot configuration

--- bitmap-utils-check/playground/server/routes/bitmapRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getBitmapReq } from '../controllers/bitmapController.js';
-
-const router = express.Router();
-
-router.get('/getBitmap/:id', getBitmapReq)
-
-export default router;
